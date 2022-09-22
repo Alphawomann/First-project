e465cdc31d3b2fc5dd10c0f03a7f50e35e8b8b7d
Alphawomann <ifureiwuo@gmail.com>
2022-09-22 22:32:14 +0100

    This is the first project and it's about an array struct

---
+++ alpha.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract array{
+
+    //Create a struct Microsft which contains name, age, height, class and genotype.
+
+    struct Microsoft{
+       string name;
+       uint age;
+       uint height;
+       string class;
+       string genotype;
+    }
+//Create an array of struct 
+ Microsoft[] Sifon;
+
+//declare a function
+function Ifure(string memory _name, uint _age, uint _height, string memory _class, string memory _genotype)public{
+
+//push your struct data into an array
+Sifon.push(Microsoft (_name, _age, _height, _class, _genotype));
+}
+
+}  
